ea04506d126f38b2ed070c93777c85ab67e084ee
Stanislav Orishkevich <orishkevich.stanislav@gmail.com>
2017-05-03 18:12:32 +0300

    реализация парсера и вывод списка

--- app/src/main/java/com/orishkevich/marvelapp/MainActivity.java
+++ app/src/main/java/com/orishkevich/marvelapp/MainActivity.java
@@ -1,17 +1,66 @@
 package com.orishkevich.marvelapp;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ContinentFragment ff;
     private FragmentTransaction fT;
-
-
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        try {
+            XmlPullParser parser = getResources().getXml(R.xml.regions);
+
+            while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+
+                for (int i = 0; i < parser.getAttributeCount(); i++) {
+                if (parser.getEventType() == XmlPullParser.START_TAG
+                        && parser.getName().equals("region")
+                        &&parser.getAttributeValue(i).equals("continent")) {
+                    for (int j = 0; j < parser.getAttributeCount(); j++) {
+                    list.add(parser.getAttributeValue(j));
+                    }
+                }
+                parser.next();
+            }}
+        } catch (Throwable t) {
+            Toast.makeText(this,
+                    "Ошибка при загрузке XML-документа: " + t.toString(), Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+    }
+    }
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -23,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         fT.addToBackStack(null);
         fT.commit();
 
-    }
-    }
+    }*/
+
 
 
